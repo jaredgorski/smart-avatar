918cd7268c012f462c94dcfa162715bf1d7081aa
jaredgorski <jared.gorski@liferay.com>
2019-02-21 18:45:33 -0800

    Fix various bugs, bump (closes #15, closes #16)

--- test/manual/gulpfile.test.js
+++ test/manual/gulpfile.test.js
@@ -1,14 +1,15 @@
-var gulp = require('gulp');
 var concat = require('gulp-concat');
+var gulp = require('gulp');
+var path = require('path');
 var replace = require('gulp-replace');
 
 gulp.task('build', function(done) {
-  gulp.src(__dirname + '/lib/**/*.js')
+  gulp.src(path.join(__dirname, '../../lib/**/*.js'))
     .pipe(replace(/function smartAvatar/g, 'export default function smartAvatar'))
     .pipe(replace(/module\.exports(.*)/g, ''))
     .pipe(replace(/var {?[\s\S]*}? = require\((.*)\);/g, ''))
     .pipe(concat('sa-test.js'))
-    .pipe(gulp.dest(__dirname + '/test/manual/js'))
+    .pipe(gulp.dest(__dirname + '/js'))
   done();
 });
 
